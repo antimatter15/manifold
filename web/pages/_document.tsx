43c20e632441d6a7d5a06218075abab6588d53a8
Marshall Polaris <marshall@pol.rs>
2022-05-07 23:47:40 -0700

    Use Next Script component the way it wants us to

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -17,21 +17,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&family=Readex+Pro:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
-
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-SSFK1Q138D"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SSFK1Q138D');
-          `,
-          }}
-        />
       </Head>
 
       <body className="font-readex-pro bg-base-200 min-h-screen">
